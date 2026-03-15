bb594e591dca3916bd9e95e09c0f2c8b030172b7
HushaamShah <hasham393@gmail.com>
2026-03-16 01:28:56 +0500

    chap 2 completed

--- main.go
+++ main.go
@@ -3,26 +3,59 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	// StripPrefix removes "/app" from the path so FileServer sees "/" or "/index.html" etc.
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		msg := "OK"
-		w.Write([]byte(msg))
-	})
-	// mux.Handle("assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", handlerHealth)
+	mux.HandleFunc("GET /metrics", cfg.getMetrics)
+	mux.HandleFunc("POST /reset", cfg.resetMetrics)
 	log.Printf("Serving on port: %s\n", port)
 	server.ListenAndServe()
 
+}
+
+func handlerHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	msg := "OK"
+	w.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	hitsStr := strconv.FormatInt(int64(hits), 10)
+	msg := "Hits: " + hitsStr
+	w.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 }
